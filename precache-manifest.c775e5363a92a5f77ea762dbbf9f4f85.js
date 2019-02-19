c4ad8365ab9a59247ba83e57fd4d77f509711a4b
sujini <ssujini0102@gmail.com>
2019-02-19 09:42:43 +0900

    Updates

---
+++ precache-manifest.c775e5363a92a5f77ea762dbbf9f4f85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7161136ce2001be468d0",
+    "url": "/github.io/english/static/js/runtime~main.7161136c.js"
+  },
+  {
+    "revision": "c7687c1eb562a7e5b0e2",
+    "url": "/github.io/english/static/js/main.c7687c1e.chunk.js"
+  },
+  {
+    "revision": "cd082662197d9967baa7",
+    "url": "/github.io/english/static/js/2.cd082662.chunk.js"
+  },
+  {
+    "revision": "c7687c1eb562a7e5b0e2",
+    "url": "/github.io/english/static/css/main.a6cfe15f.chunk.css"
+  },
+  {
+    "revision": "caea65c5c7f451d26cc77e8d1cc39906",
+    "url": "/github.io/english/index.html"
+  }
+];
